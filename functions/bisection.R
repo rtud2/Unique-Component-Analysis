d29a21b73a57f137bc62b9bfc7506fd2509db052
Robin Tu <robintu2@illinois.edu>
2020-03-03 11:44:19 -0600

    add function bisection.multiple to be able to calculate lagrangian for multiple background data's

--- functions/bisection.R
+++ functions/bisection.R
@@ -1,50 +1,85 @@
-#' bisection: bisection's method
+#' score_calc: calculating the derivative for specific value of lagrangian
 #'
-#' Use bisection method to find the optimal Lagrangian for solving discriminant PCA. depends on irlba
+#' Calculate the derivative of Lagrangian. depends on irlba
 #' 
 #' @param A dataset - dataset of interest
 #' @param B Background dataset(s). Input multiple datasets as a list. 
 #' @param nv number of eigenvectors to use
-#' @param lower lowerbound for root finding
-#' @param upper upperbound for root finding
+#' @param tau lowerbound for root finding
+#' @return list of tau and value of derivative, the optimal lagrangian
+
+
+score_calc = function(A, B, nv, tau, return_all = F){
+  eigen_calc <- partial_eigen ( A - tau * B, n = nv, symmetric = T)
+  v <- matrix(eigen_calc$vectors, ncol = nv)
+  score = 1 - sum(diag(crossprod(v, B)%*% v)) #derivative of  sum(diag(crossprod(v, (A - tau * B) %*% v))) + tau
+  if(return_all){
+    list(vector = v, score = score, tau = tau, nv = nv)
+  }else{
+    return(score)
+    
+  }
+  }
+
+#' bisection
+#'
+#' Use bisection method to find the optimal Lagrangian for solving discriminant PCA. depends on irlba
+#' 
+#' @param A dataset - dataset of interest
+#' @param B Background dataset(s). 
+#' @param nv number of eigenvectors to use
+#' @param limit a vector of c(lower, upper) bounds
 #' @param tol tolerance for when to stop the algorithm
 #' @return tau, the optimal lagrangian
 
-
-score_calc = function(A, B, nv, tau){
-  eigen_calc <- partial_eigen ( A - tau * B, n = nv, symmetric = T)
-  v <- matrix(eigen_calc$vectors, ncol = nv)
-  score = 1 - sum(diag(crossprod(v, B %*% v))) #derivative of  sum(diag(crossprod(v, (A - tau * B) %*% v))) + tau
-  return(
-    list( score = score, tau = tau)) #v = v))
-}
-
-
-bisection = function(A, B, nv = 1, lower = 0, upper = 100, tol = 1E-5, lower_score = NULL, upper_score = NULL){
+bisection = function(A, B, nv = 1, limit = c(0,100), maxit = 1E5, tol = 1E-4, checks = F){
   
-  if(length(lower_score)==0 & length(upper_score)==0 ){
-    upper_score = score_calc(A, B, nv = nv, tau = upper)
-    lower_score = score_calc(A, B, nv = nv, tau = lower)
-    
-    if(sign(upper_score$score) + sign(lower_score$score) !=0){
-      stop("bisection will not work with the lower and upper values chosen")
+  if(checks == T){
+    if(sign(score_calc(A, B, nv = nv, tau = upper)$score) + sign(score_calc(A, B, nv = nv, tau = lower)$score) !=0){
+      stop("bisection will not work with the initial lower and upper values chosen.")
+    }
+    if(upper < lower){
+      stop("upper value must be larger than lower value")
     }
   }
+
+  for(iter in 1:maxit){
+    if( max(limit) - min(limit) < tol) break;
+    
+    tau = sum(limit)/2
+    if(score_calc(A, B, nv = nv, tau = tau) < 0){
+      limit[1] = tau
+    }else{
+      limit[2] = tau
+    }
+  }
+  final = score_calc(A, B, nv = nv, tau = tau, return_all = T) 
+   
+  return(final)
+}
+
+#' bisection.multiple: bisection method but for multiple background datasets
+#'
+#' Use bisection method to find the optimal Lagrangian for solving discriminant PCA of each background dataset. depends on irlba
+#' 
+#' @param A dataset - dataset of interest
+#' @param B list of background dataset(s). 
+#' @param ... other parameters to pass in to bisection(...)
+#' @return tau, the optimal lagrangian
+
+
+bisection.multiple = function(A, B, ...){
+ if(!is.list(B)){
+   stop("B is not a list of matrices")
+ }else if(!is.list(A)){
+   warning(paste0("replicating A ", length(B), " times to match length(B)"))
+   A = replicate(n = length(B),expr =  A, simplify = F)
+ }
   
-  prev_score <- c(lower_score$score, upper_score$score)
-  
-  tau = (lower_score$tau + upper_score$tau)/2
-  proposed_score = score_calc(A, B, nv = nv, tau = tau)
-  
-  to_replace = which(sign(prev_score) == sign(proposed_score$score))
-  score_diff = as.numeric(proposed_score$score - prev_score[-to_replace])
-  
-  if(abs(score_diff) > tol & to_replace == 1){  
-    bisection(A, B, lower_score = proposed_score, upper_score = upper_score)
-  }else if(abs(score_diff) > tol & to_replace == 2){
-    bisection(A, B, lower_score = lower_score, upper_score = proposed_score)
+  if( length(list(...)) > 0){
+    mapply(FUN = bisection, A, B, list(...), SIMPLIFY = F)
   }else{
-      return(tau)
+    mapply(FUN = bisection, A, B, SIMPLIFY = F)
   }
 }
 
